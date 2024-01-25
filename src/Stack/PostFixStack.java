02ffeab4467d57d1401460b517c41bd77e62f406
Pedro Avila <123851480+PeDro0210@users.noreply.github.com>
2024-01-25 02:57:36 +0000

    Stack Commit

--- src/Stack/PostFixStack.java
+++ src/Stack/PostFixStack.java
@@ -1,4 +1,31 @@
 package Stack;
-public class PostFixStack {
 
+import java.util.Vector;
+
+public class PostFixStack<G> {
+
+    private Vector<G> stack;
+
+    public PostFixStack() {
+        stack = new Vector<G>();
+    }
+
+    public void push(G item) {
+        stack.add(item);
+    }
+
+    public G pop() {
+        G item = stack.lastElement();
+        stack.remove(stack.size() - 1);
+        return item;
+    }
+
+    public G top() {
+        return stack.lastElement();
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+    
 }
